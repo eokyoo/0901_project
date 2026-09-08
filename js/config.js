f90561a3fd6caf499d4d106dc9fda02dd6b526d6
eokyoo <325885083+eokyoo@users.noreply.github.com>
2026-09-08 15:48:06 +0900

    Point blog auth to corrected Apps Script deployment

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwhr6mNm5riCD3XRz9ZnfQ4PXJfQLLwWKRDngVm8XLEJIclhxhAj4hzW9O9514t3R3S/exec'
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbwJ-T5BfvaXdOF3K85IGXBoA0dFvv4nmaCb2dFtEcYkycuMFzsVdxeAJRtaBcmqrLr9/exec'
 };
