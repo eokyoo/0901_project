ac920ac0e2e1f5d6901fab9b0821e92b3964b956
eokyoo <325885083+eokyoo@users.noreply.github.com>
2026-09-14 14:28:59 +0900

    Use verified blog CRUD deployment

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 window.APP_CONFIG = {
-  appsScriptUrl: 'https://script.google.com/macros/s/AKfycbxiwaFHCqSWTG-4nCUn3qe-Q5rvXjXGl6Ftb2GyK7zmG2qIYmMJMdIG9RehxfxiGPNC/exec'
+  appsScriptUrl: 'https://script.google.com/macros/s/AKfycby4ZI-LdOtMV3VpiJQBC1Q7yDDTiDA7By0QHHu7bH7scSpoxhUikAjWq0BSea19jrFW/exec'
 };
